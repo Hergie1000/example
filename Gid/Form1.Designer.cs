3e36269984644aa7ccf8c35749ba431b4f08bf34
Herby <msterwhite@a1.net>
2017-06-08 11:49:41 +0200

    noch eines

--- Gid/Form1.Designer.cs
+++ Gid/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.treeView1 = new System.Windows.Forms.TreeView();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // dateTimePicker1
@@ -55,11 +56,20 @@
             this.treeView1.Size = new System.Drawing.Size(352, 295);
             this.treeView1.TabIndex = 2;
             // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(486, 303);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(833, 511);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.treeView1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dateTimePicker1);
@@ -74,6 +84,7 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
